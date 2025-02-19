8fb39a33b4f76a6c1d8a0d6893be873b7848122a
Oliver C. Stringham <oliverstringham@gmail.com>
2025-02-19 16:38:43 -0500

    Create wk-04-live.sh

---
+++ Week-04-trim-ont-adaters-to-motus/wk-04-live.sh
@@ -0,0 +1,78 @@
+
+
+
+## deduplicate linked reads (just in case)
+singularity exec images/seqkit.sif \
+seqkit rmdup data/florida_cf/barcodes-04-10.q10.l300.L400.mifish_linked_all_v2.fastq > \
+data/florida_cf/barcodes-04-10.q10.l300.L400.mifish_linked.dd.fastq
+
+## get IDs of linked reads (needed to subset)
+awk 'NR%4==1 {sub(/^@/, "", $1); print $1}' \
+data/florida_cf/barcodes-04-10.q10.l300.L400.mifish_linked.dd.fastq > \
+data/florida_cf/barcodes-04-10.q10.l300.L400.mifish_linked.dd.ids.txt
+
+## remove linked reads from unlinked reads
+singularity exec images/seqkit.sif \
+seqkit grep --invert-match \
+-f data/florida_cf/barcodes-04-10.q10.l300.L400.mifish_linked.dd.ids.txt \
+data/florida_cf/barcodes-04-10.q10.l300.L400.mifish_unlinked_all_v2.fastq | \
+singularity exec images/seqkit.sif seqkit rmdup > \
+data/florida_cf/barcodes-04-10.q10.l300.L400.mifish_unlinked_all_v2.sub.fastq
+
+# shuffle unlinked reads 
+singularity exec images/bbmap.sif \
+shuffle.sh in=data/florida_cf/barcodes-04-10.q10.l300.L400.mifish_unlinked_all_v2.sub.fastq \
+out=data/florida_cf/barcodes-04-10.q10.l300.L400.mifish_unlinked_all_v2.sub.shuf.fastq
+
+singularity exec images/porechop.sif \
+porechop --threads 8 --discard_middle \
+-i data/florida_cf/barcodes-04-10.q10.l300.L400.mifish_unlinked_all_v2.sub.shuf.fastq > \
+data/florida_cf/barcodes-04-10.q10.l300.L400.mifish_unlinked_all_v2.sub.shuf.porechop.fastq
+
+
+
+# Combine linked and unlinked reads
+cat data/florida_cf/barcodes-04-10.q10.l300.L400.mifish_linked.dd.fastq \
+data/florida_cf/barcodes-04-10.q10.l300.L400.mifish_unlinked_all_v2.sub.shuf.porechop.fastq > \
+data/florida_cf/barcodes-04-10.q10.l300.L400.mifish_linked_unlinked.fastq
+
+# rm duplicates just in case
+singularity exec images/seqkit.sif \
+seqkit rmdup data/florida_cf/barcodes-04-10.q10.l300.L400.mifish_linked_unlinked.fastq > \
+data/florida_cf/barcodes-04-10.q10.l300.L400.mifish_linked_unlinked.dd.fastq
+
+
+## convert to fasta
+singularity exec images/seqkit.sif \
+seqkit fq2fa data/florida_cf/barcodes-04-10.q10.l300.L400.mifish_linked_unlinked.dd.fastq \
+-o data/florida_cf/barcodes-04-10.q10.l300.L400.mifish_linked_unlinked.dd.fasta
+
+head data/florida_cf/barcodes-04-10.q10.l300.L400.mifish_linked_unlinked.dd.fasta
+
+## tabulate
+### Drop attributes except barcode
+awk '/^>/ {split($0, a, " "); printf("%s", a[1]); for (i=2; i<=length(a); i++) { if (match(a[i], /^barcode=/)) printf(" %s", a[i]); } printf("\n"); next} {print}' \
+data/florida_cf/barcodes-04-10.q10.l300.L400.mifish_linked_unlinked.dd.fasta > \
+data/florida_cf/barcodes-04-10.q10.l300.L400.mifish_linked_unlinked.dd.att.fasta
+
+head data/florida_cf/barcodes-04-10.q10.l300.L400.mifish_linked_unlinked.dd.att.fasta
+
+singularity exec images/seqkit.sif \
+seqkit fx2tab -l data/florida_cf/barcodes-04-10.q10.l300.L400.mifish_linked_unlinked.dd.att.fasta \
+-o data/florida_cf/barcodes-04-10.q10.l300.L400.mifish_linked_unlinked.dd.att.txt
+
+
+
+# Clustering to MOTUs w/vsearch
+
+## mkdir to vsearch outputs
+mkdir data/florida_cf/vsearch
+
+## call vsearch cluster_fast
+singularity exec images/vsearch.sif \
+vsearch --threads 4 \
+--cluster_fast data/florida_cf/barcodes-04-10.q10.l300.L400.mifish_linked_unlinked.dd.att.fasta \
+--id 0.98 \
+--centroids data/florida_cf/vsearch/barcodes-04-10.q10.l300.L400.mifish_linked_unlinked.dd.cen.fasta \
+--consout data/florida_cf/vsearch/barcodes-04-10.q10.l300.L400.mifish_linked_unlinked.dd.con.fasta \
+--uc data/florida_cf/vsearch/barcodes-04-10.q10.l300.L400.mifish_linked_unlinked.dd.clusters.uc
