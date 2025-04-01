5dc18f94b1050c6d27568591f062637d5d11db74
Oliver C. Stringham <oliverstringham@gmail.com>
2025-04-01 17:43:18 -0400

    start command line r call

--- Week-06-clean-blast/wk-06.sh
+++ Week-06-clean-blast/wk-06.sh
@@ -4,20 +4,25 @@
 
 # download ncbi_lineages_2025-01-08_fish.csv and move it to blastdb directory
 
+# Download R image with packages installed
+singularity build images/r_env.sif docker://olistr12/r_env:0.0.7
 
+# make new dir to store results
+mkdir data/florida_cf/blast
 
 # run R script
+singularity exec images/r_env.sif \
 Rscript scripts/blast_compile_results_CL.R \
---cluster_key "$vsearch_uc" \
---barcode_key "$reads_tab" \
---conseq_key "$vsearch_con_sub_tab_edit" \
---blast_results "$blast_output" \
+--cluster_key data/florida_cf/vsearch/barcodes-04-10.q10.l300.L400.mifish_linked_unlinked.dd.clusters.uc \
+--barcode_key data/florida_cf/barcodes-04-10.q10.l300.L400.mifish_linked_unlinked.dd.att.txt \
+--conseq_key data/florida_cf/vsearch/barcodes-04-10.q10.l300.L400.mifish_linked_unlinked.dd.con.sub2.txt \
+--blast_results data/florida_cf/vsearch/barcodes-04-10.q10.l300.L400.mifish_linked_unlinked.dd.con.sub.blastn.csv \
 --sample_key "$sample_key" \
 --primer_name "MiFish" \
 --min_alignment_length 100 \
 --local_global "local" \
---output_cluster_filepath "$output_cluster_filepath" \
---output_taxa_filepath "$output_taxa_filepath" \
---clusters_matched_filepath "$clusters_matched_filepath" \
+--output_cluster_filepath data/florida_cf/blast/blast_local.MiFish.vs-cluster-0.98.csv \
+--output_taxa_filepath data/florida_cf/blast/blast_local.MiFish.vs-cluster-0.98.taxa.csv \
+--clusters_matched_filepath data/florida_cf/blast/blast_local.MiFish.vs-cluster-0.98.clusters_matched.txt \
 --taxdump_filepath blastdb/ncbi_lineages_2025-01-08_fish.csv \
---custom_taxa_filepath "$local_custom_taxa_loc"
+--custom_taxa_filepath refdb/florida/refdb_florida_fish_dl_2025-03-26.csv
